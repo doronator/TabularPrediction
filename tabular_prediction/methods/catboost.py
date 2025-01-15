48defd035d34133163a9ff7310cf6c85328aef2b
bqk928 <doron.bergman@capitalone.com>
2025-01-15 16:56:10 +0000

    enable using gpu for catboost

--- tabular_prediction/methods/catboost.py
+++ tabular_prediction/methods/catboost.py
@@ -5,6 +5,8 @@ import numpy as np
 
 from hyperopt import hp
 
+from catboost import CatBoostClassifier, CatBoostRegressor
+
 from tabular_prediction.utils import is_classification, make_pd_from_np, preprocess_impute, eval_complete_f
 
 MULTITHREAD = -1
@@ -29,8 +31,6 @@ def get_scoring_string(metric_used):
         raise Exception('No scoring string found for metric')
 
 def catboost_predict(x, y, test_x, test_y, metric_used, cat_features=None, max_time=300, no_tune=None, gpu_id=None):
-    from catboost import CatBoostClassifier, CatBoostRegressor
-
     x, y, test_x, test_y, cat_features = preprocess_impute(x, y, test_x, test_y,
         one_hot=False, impute=False, standardize=False, cat_features=cat_features)
 
@@ -40,7 +40,9 @@ def catboost_predict(x, y, test_x, test_y, metric_used, cat_features=None, max_t
     )
 
     if gpu_id is not None:
-        gpu_params = {'task_type':"GPU", 'devices':gpu_id}
+        gpu_params = {'task_type':"GPU"
+                    #   , 'devices':gpu_id
+                      }
     else:
         gpu_params = {}
 
