12ef797b138181b2ef09c63f2a3ac85a736854fd
bqk928 <doron.bergman@capitalone.com>
2025-01-16 03:53:29 +0000

    modernized ridge

--- classification/cli_ridge_classification.py
+++ classification/cli_ridge_classification.py
@@ -5,13 +5,20 @@ from tabular_prediction.methods import ridge_predict
 from tabular_prediction.metrics import accuracy_metric, balanced_accuracy_metric, cross_entropy_metric, auc_metric
 
 from read_data import get_datasets
+from handle_results import prepare_results_file, write_results
     
 def run_evaluation(split):
     max_time = [1, 5, 10, 30, 60, 120, 300, 600, 3600]
 
     data_dir, datasets = get_datasets(split)
-
-    with open(f"../results/ridge-classification-{split}.csv", "a") as f:
+    
+    result_file = f"../results/ridge-classification-{split}.csv"
+    
+    previous_results = prepare_results_file(result_file)
+    if previous_results is None:
+        exit()
+        
+    with open(result_file, "a") as f:
         f.write(','.join(["dataset", "acc", "bacc", "ce", "auc", "time"]))
         f.write('\n')
         f.flush()
@@ -21,22 +28,24 @@ def run_evaluation(split):
             cat_features = torch.where(data["cat_features"])[0].to(torch.int32)
 
             test_y, summary, _ = ridge_predict(x_train, y_train, x_test, y_test, cat_features=cat_features, metric_used=cross_entropy_metric, max_time=max_time)
-            for stop_time in summary:
-                pred = summary[stop_time]['pred']
-                run_time = summary[stop_time]['tune_time'] + summary[stop_time]['train_time'] + summary[stop_time]['predict_time']
-                f.write(','.join([dataset] + [f'{val:5.4f}' for val in [accuracy_metric(test_y, pred), balanced_accuracy_metric(test_y, pred), cross_entropy_metric(test_y, pred), auc_metric(test_y, pred), run_time]]))
-                f.write('\n')
-                f.flush()
+            write_results(test_y, summary, max_time, dataset, file_handler=f)                
                 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('split', type=int, help='split number - must be 1 to 6')
+parser.add_argument('--split', type=int, help='split number - must be 1 to 6', default=1)
+parser.add_argument('-m', '--multi_processing', action='store_true', help='whether to use python multiprocessing - this tries to spawn all splits from one python process')
+
 args = parser.parse_args()
 
-split = args.split
-assert split in range(1,7)
-
-print(f"starting split {split}")
-run_evaluation(split)
-print(f"completed split {split}")
+if args.multi_processing:
+    print("Running with multiprocessing!")
+    with Pool(processes=6) as p:
+        print(p.map(run_evaluation, range(1, 7)))
+else:
+    split = args.split
+    assert split in range(1,7)
+    
+    print(f"starting split {split}")
+    run_evaluation(split=split)
+    print(f"completed split {split}")
 
