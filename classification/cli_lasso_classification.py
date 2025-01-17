70bb4146c66e8491915ba29d640bef38e3f5ae99
bqk928 <doron.bergman@capitalone.com>
2025-01-17 15:29:29 +0000

    lifting 5k limit on lasso and ridge

--- classification/cli_lasso_classification.py
+++ classification/cli_lasso_classification.py
@@ -31,10 +31,10 @@ def run_evaluation(split):
                 continue
             x_train, y_train, x_test, y_test = data["data"]
                         
-            total_num_of_samples = (x_train.shape[0] + x_test.shape[0])
-            if total_num_of_samples > 5000:
-                print(f"Skipping {dataset} total_num_of_samples={total_num_of_samples}")
-                continue
+            # total_num_of_samples = (x_train.shape[0] + x_test.shape[0])
+            # if total_num_of_samples > 5000:
+            #     print(f"Skipping {dataset} total_num_of_samples={total_num_of_samples}")
+            #     continue
             
             if dataset in previous_results:
                 assert previous_results.loc[dataset] == len(max_time)
